215294604be7b5e667e0138818a47016d7e971a6
Do0oMores <87219087+mores1420@users.noreply.github.com>
2023-04-03 02:30:49 +0800

    create

---
+++ DaMeng-BedrockRankingData/src/main/java/cn/mores/Kook_RankingData.java
@@ -0,0 +1,10 @@
+package cn.mores;
+
+import snw.jkook.plugin.BasePlugin;
+
+public class Kook_RankingData extends BasePlugin {
+    @Override
+    public void onEnable(){
+        getLogger().info("排行榜数据插件已成功加载！");
+    }
+}
